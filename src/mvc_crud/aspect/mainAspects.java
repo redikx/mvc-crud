841379c736d3713dbdaf985eaba3548eb803521f
redik <redikx@gmail.com>
2018-09-23 23:55:35 +0200

    Added aspects for Delete and Update Customer

--- src/mvc_crud/aspect/mainAspects.java
+++ src/mvc_crud/aspect/mainAspects.java
@@ -1,7 +1,9 @@
 package mvc_crud.aspect;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -16,9 +18,40 @@ public class mainAspects {
 	public void ListCustomerAspect() {}
 	
 	@Before("mvc_crud.aspect.mainAspects.ListCustomerAspect()")
-	public void exec1() {
+	public void BeforeListCustomerAspect() {
 		System.out.println("List Customers " + LocalDateTime.now());
 	}
+	
+	@Pointcut("execution(* mvc_crud.dao.CustomerDAO.saveCustomer(..))")
+	public void SaveCustomerAspect() {}
+	
+	@After("SaveCustomerAspect()")
+	public void AfterSaveCustomerAspect(JoinPoint jointPoint) {
+		String theMethod = jointPoint.getSignature().toString();
+		System.out.println("Signature : " + theMethod);
+		Object[] args = jointPoint.getArgs();
+		
+		for (Object tempargs : args) {
+			System.out.println(tempargs.toString());
+		} 
+	}
+	
+	@Pointcut("execution(* mvc_crud.dao.CustomerDAO.deleteCustomer(..))")
+	public void DeleteCustomerAspect() {}
+	
+	@Before("DeleteCustomerAspect()") 
+	public void BeforeDeleteCustomerAspect(JoinPoint joinPoint) {
+		String theMethod = joinPoint.getSignature().toString();
+		System.out.println("Signature " + theMethod + " at " + LocalDateTime.now());
+		Object[] args = joinPoint.getArgs();
+		
+		for (Object tempargs : args) {
+			System.out.println("Customer " + tempargs.toString() + " deleted");
+			
+		}
+				
+	}
+	
 	
 	
 }
--- src/mvc_crud/controllers/CustomerController.java
+++ src/mvc_crud/controllers/CustomerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import mvc_crud.dao.CustomerDAO;
 import mvc_crud.entities.Customer;
 import mvc_crud.services.CustomerService;
 
